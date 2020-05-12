eae148deed64074b84fc345e3b84f73702ebdc65
Fowley <alexanderfowle@gmail.com>
2020-05-12 14:55:02 -0500

    Add project files.

---
+++ LINQStuff/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LINQStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solutions sln = new Solutions();
+
+            //List<string> lst = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            //sln.FoundATh(lst);
+            
+            //List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            //sln.NoDuplicates(names);
+
+
+        }
+    }
+}
---
+++ LINQStuff/Solutions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LINQStuff
+{
+    class Solutions
+    {
+        public List<string> FoundATh(List<string> list)
+        {
+            var wordsWithTh = new List<string>();
+            var th = "th";
+            wordsWithTh = list.Where(x => x.Contains(th)).ToList();
+            return wordsWithTh;
+        }
+
+        public List<string> NoDuplicates(List<string> list)
+        {
+            var noDupes = new List<string>();
+            noDupes = list.Distinct().ToList();
+            return noDupes;
+        }
+    }
+}
